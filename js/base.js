89764237f621dbb8e13e0d7b40bd3a443c3d0a7a
Igor Padoim <igorpadoim@gmail.com>
2021-06-20 22:19:30 -0300

    Adicionar página HTML de Exame Psíquico

--- js/base.js
+++ js/base.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   const year = "2021";
   const license_name = "Licença Hipocrática";
   const license_url = "https://firstdonoharm.dev/";
-  const version = "1.12.11-r10";
+  const version = "1.13.11-r10";
 
   $(".copy.name").html(name + " &ndash; " + $(".copy.name").html());
   $(".copy.author").html(author);
