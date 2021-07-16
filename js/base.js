e482b67133af05645ef72771df83018d83c5eabe
Igor Padoim <igorpadoim@gmail.com>
2021-07-16 09:10:12 -0300

    Adicionar alguns dados de qualidade-de-vida Adicionados queixa de obstipação aos Sintomas Comuns e diagnóstico de hipotireoidismo aos Diagnósticos Rápidos

--- js/base.js
+++ js/base.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   const year = "2021";
   const license_name = "Licença Hipocrática";
   const license_url = "https://firstdonoharm.dev/";
-  const version = "1.17.15-r12";
+  const version = "1.17.15-r13";
 
   $(".copy.name").html(name + " &ndash; " + $(".copy.name").html());
   $(".copy.author").html(author);
