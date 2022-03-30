0915cf1e8dc06b17ff610a5c48419ab816a921b1
Igor Padoim <igorpadoim@gmail.com>
2022-03-29 22:24:56 -0300

    Mover metadados do aplicativo

--- js/base.js
+++ js/base.js
@@ -14,17 +14,4 @@ Date.prototype.getDOY = function() {
     return dayOfYear;
 };
 
-$(document).ready(function() {
-  const name = "Modelo de Prontuários";
-  const author = "Igor Padoim";
-  const year = "2021-2022";
-  const license_name = "Licença Hipocrática";
-  const license_url = "https://firstdonoharm.dev/";
-  const version = "1.25.22-r22";
-
-  $(".copy.name").html(name + " &ndash; " + $(".copy.name").html());
-  $(".copy.author").html(author);
-  $(".copy.year").html(year);
-  $(".copy.license").html(license_name).attr("href", license_url);
-  $(".copy.version").html(version);
-});
+$(document).ready(function() {});
---
+++ meta.php
@@ -0,0 +1,8 @@
+<?php
+define("APP_NAME", "Modelo de Prontuários");
+define("APP_AUTHOR", "Igor Padoim");
+define("APP_YEAR", "2021-2022");
+define("APP_LICENSE_NAME", "Licença Hipocrática");
+define("APP_LICENSE_URI", "https://firstdonoharm.dev/");
+define("APP_VERSION", "2.0.0");
+?>
