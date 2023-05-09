ee5d48d06d3f260e387e67cb1d894790837180ad
Igor Padoim <igorpadoim@gmail.com>
2023-05-09 12:02:06 -0300

    Adicionar função de capitalização de 1ª letra

--- js/base.js
+++ js/base.js
@@ -1,19 +1,3 @@
-Date.prototype.isLeapYear = function() {
-    var year = this.getFullYear();
-    if((year & 3) != 0) return false;
-    return ((year % 100) != 0 || (year % 400) == 0);
-};
-
-// Get Day of Year
-Date.prototype.getDOY = function() {
-    var dayCount = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
-    var mn = this.getMonth();
-    var dn = this.getDate();
-    var dayOfYear = dayCount[mn] + dn;
-    if(mn > 1 && this.isLeapYear()) dayOfYear++;
-    return dayOfYear;
-};
-
 $(function() {
   console.log("Main Logic Ready!");
   new ClipboardJS("#insulin");
@@ -76,6 +60,11 @@ Date.prototype.getDOY = function () {
   if (mn > 1 && this.isLeapYear()) dayOfYear++;
   return dayOfYear;
 };
+
+// Capitalize first letter
+String.prototype.firstLettertoUpperCase = function () {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+}
 
 // Makeshift Uuid Generation
 function uuidPoor() {
