c779fa03c6d3200d5b4b0147c1b56eb3393796e1
Igor Padoim <igorpadoim@gmail.com>
2025-10-28 12:30:31 -0300

    Remontar estilo, usando Bulma v1

--- assets/pront.js
+++ assets/pront.js
@@ -1,1 +1,34 @@
+// SCSS imports
 import './pront.scss';
+
+/******************************************/
+/*                   JS                   */
+/******************************************/
+// Base Methods
+$(function () {
+  console.log("Page ready!");
+
+  // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+  $(".navbar-burger").on("click", function () {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
+
+  // Check if required fields are filled
+  $("[required]").on("change input", function () {
+    // Change color code if missing data
+    if ($(this).val() == "" || $(this).val() == null) {
+      if ($(this).prop("nodeName").toLowerCase() === "select") {
+        $(this).parent().addClass("is-danger");
+      } else {
+        $(this).addClass("is-danger");
+      }
+    } else {
+      if ($(this).prop("nodeName").toLowerCase() === "select") {
+        $(this).parent().removeClass("is-danger");
+      } else {
+        $(this).removeClass("is-danger");
+      }
+    }
+  }).trigger("change");
+});
