f8b2ecf68aa2d341b5ef761a89a19df16680905e
Igor Padoim <igorpadoim@gmail.com>
2025-10-29 11:30:15 -0300

    Criar filtro para números de celular brasileiros

---
+++ src/Twig/FormatPhone.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Attribute\AsTwigFilter;
+
+class FormatPhone
+{
+  #[AsTwigFilter('format_phone')]
+  public function formatPhone(string $phone): string
+  {
+    preg_match('/^(\+\d{1,3})(\d{2})(\d{5})(\d{4})$/', $phone, $phone);
+
+    return implode(' ', array_slice($phone, 1));
+  }
+}
