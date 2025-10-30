2a65d6331d49f6f7265f746a52964aa5b6830da8
Igor Padoim <igorpadoim@gmail.com>
2025-10-29 21:37:29 -0300

    Refatorar para valores `null` de telefones

--- src/Twig/FormatPhone.php
+++ src/Twig/FormatPhone.php
@@ -7,8 +7,9 @@ use Twig\Attribute\AsTwigFilter;
 class FormatPhone
 {
   #[AsTwigFilter('format_phone')]
-  public function formatPhone(string $phone): string
+  public function formatPhone(?string $phone): string
   {
+    $phone = is_null($phone) ? '' : $phone;
     preg_match('/^(\+\d{1,3})(\d{2})(\d{5})(\d{4})$/', $phone, $phone);
 
     return implode(' ', array_slice($phone, 1));
